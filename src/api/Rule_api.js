603c5af0aff87a12926b5c375e44d4007b5317b3
Mariano Camacho <macadema@gmail.com>
2023-01-06 17:29:23 -0300

    preparando para vercel. entorno seteado en rule_api

--- src/api/Rule_api.js
+++ src/api/Rule_api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:3001",
+  baseURL: import.meta.env.REACT_APP_ENV,
   headers: {
     "Content-Type": "application/json",
   },
